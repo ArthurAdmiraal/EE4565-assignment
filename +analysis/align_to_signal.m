6856b46153462b94a212632a9bf955a0838bbf8b
ArthurAdmiraal <arthuradmiraal@hotmail.nl>
2022-01-03 15:08:42 +0100

    Add masking to signal alignment

--- +analysis/align_to_signal.m
+++ +analysis/align_to_signal.m
@@ -1,4 +1,5 @@
-function signal_aligned = align_to_signal(signal_ref, signal_to_align)
+function signal_aligned = align_to_signal(t_ns, signal_ref, signal_to_align, mask_range)
+  signal_ref( (t_ns >= mask_range(1)) & (t_ns <= mask_range(2)) ) = 0;
   [r, lags]  = xcorr(signal_ref, signal_to_align);
   [M,I]      = max(r);
   to_shift   = lags(I);
